c32eb0f972400b4f484e446604b3373d0803a465
munnaaaaaaaaaaaaaaaaaaaaaaa1-maker <munnaaaaaaaaaaaaaaaaaaaaaaa1@gmail.com>
2025-12-14 20:20:14 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,16 @@
+import "./globals.css";
+
 export const metadata = {
   title: "SpaceMC Store",
-  description: "Official SpaceMC Rank Store"
+  description: "Official SpaceMC Minecraft Rank Store",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-[#0f1117] text-white">
+        {children}
+      </body>
     </html>
   );
 }
